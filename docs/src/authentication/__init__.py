dfd11cbbaf7da01f77c0ed751555d7d622a1e317
Steve Piercy <web@stevepiercy.com>
2015-11-25 00:32:53 -0800

    - update authentication.rst - add src/authentication to show source code for each step in the tutorial - use literal includes to be in sync with the actual code in the src files - grammar, .rst syntax fixes, rewrap 79 cols

---
+++ docs/src/authentication/__init__.py
@@ -0,0 +1,33 @@
+from pyramid.config import Configurator
+from pyramid.authentication import AuthTktAuthenticationPolicy
+from pyramid.authorization import ACLAuthorizationPolicy
+from sqlalchemy import engine_from_config
+
+from .models.meta import (
+    DBSession,
+    Base,
+    )
+
+
+def main(global_config, **settings):
+    """ This function returns a Pyramid WSGI application.
+    """
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.bind = engine
+    authentication_policy = AuthTktAuthenticationPolicy('somesecret',
+                                                        hashalg='sha512')
+    authorization_policy = ACLAuthorizationPolicy()
+    config = Configurator(settings=settings,
+                          authentication_policy=authentication_policy,
+                          authorization_policy=authorization_policy
+                          )
+    config.include('pyramid_jinja2')
+    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_route('home', '/')
+    config.add_route('blog', '/blog/{id:\d+}/{slug}')
+    config.add_route('blog_action', '/blog/{action}',
+                     factory='pyramid_blogr.security.BlogRecordFactory')
+    config.add_route('auth', '/sign/{action}')
+    config.scan()
+    return config.make_wsgi_app()
---
+++ docs/src/authentication/models/__init__.py
@@ -0,0 +1,2 @@
+from .user import User
+from .blog_record import BlogRecord
